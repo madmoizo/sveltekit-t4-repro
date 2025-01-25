c93fc03e27a5603c4496d488bb9bdc28502ddfd0
madmoizo <matthieu.dambrune@gmail.com>
2025-01-25 00:50:59 +0000

    lint

--- vite-plugin-tailwindcss-cleaner.ts
+++ vite-plugin-tailwindcss-cleaner.ts
@@ -7,7 +7,7 @@ export default function tailwindcssCleaner(): Plugin {
 			for (const [fileName, file] of Object.entries(bundle)) {
 				if (file.type === "asset" && fileName.endsWith(".css") && typeof file.source === "string") {
 					const logs = {}
-					
+
 					// Remove unused custom properties
 					let loopCount = 0
 					let removedCustomPropertiesCount = 0
@@ -19,7 +19,7 @@ export default function tailwindcssCleaner(): Plugin {
 						const customProperties = file.source.matchAll(/(--[a-zA-Z0-9_-]+):/g)
 
 						if (customProperties) {
-							for (const [,customProperty,] of customProperties) {
+							for (const [, customProperty] of customProperties) {
 								detectedCustomPropertiesCount++
 								// lightningcss polyfills light-dark function
 								// https://lightningcss.dev/transpilation.html
@@ -37,33 +37,35 @@ export default function tailwindcssCleaner(): Plugin {
 						}
 
 						logs[`Custom properties: ${loopCount} pass`] = {
-							"Total": detectedCustomPropertiesCount,
-							"Unused": removedCustomPropertiesCount,
+							Total: detectedCustomPropertiesCount,
+							Unused: removedCustomPropertiesCount
 						}
 					} while (removedCustomPropertiesCount > 0)
 
-					
 					// Remove unused keyframes
 					let detectedKeyframesCount = 0
 					let removedKeyframesCount = 0
 					const keyframes = file.source.matchAll(/@keyframes ([a-zA-Z0-9_-]+)\{/g)
 
 					if (keyframes) {
-						for (const [,keyframe,] of keyframes) {
+						for (const [, keyframe] of keyframes) {
 							detectedKeyframesCount++
 							const used = new RegExp(`--animate-${keyframe}:`).test(file.source)
 							if (!used) {
-								file.source = file.source.replace(new RegExp(`@keyframes ${keyframe}\{(?:[^{}]|\{[^{}]*\})*\}`, "g"), "")
+								file.source = file.source.replace(
+									new RegExp(`@keyframes ${keyframe}\{(?:[^{}]|\{[^{}]*\})*\}`, "g"),
+									""
+								)
 								removedKeyframesCount++
 							}
 						}
 					}
 
-					logs['Keyframes'] = {
-						"Total": detectedKeyframesCount,
-						"Unused": removedKeyframesCount
+					logs["Keyframes"] = {
+						Total: detectedKeyframesCount,
+						Unused: removedKeyframesCount
 					}
-					
+
 					console.log("\x1b[34m%s\x1b[0m", `\n\n[plugin] Tailwindcss Cleaner`)
 					console.table(logs)
 					console.log(`\n`)
